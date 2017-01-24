c82c5f6433caa18f916e5675682b94683a8f092e
toni <toni@gomordax.info>
2017-01-24 21:46:58 +0100

    Dominio

---
+++ Apsogui.Dominio/Servicios/Alumno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Apsogui
+{
+    public class Alumno
+    {
+
+        private string m_IdAlumno="";
+        private string m_Nombre = "";
+        private string m_Apellido1="";
+        private string m_Apellido2 ="";
+        private string m_NumeroIdentificacion = "";
+        private string m_TipoIdentificacion = "";
+        private DateTime? m_FechaNacimiento = null;
+
+        public Alumno() {
+
+        }
+
+        public string IdAlumno { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string NumeroIdentificacion { get; set; }
+        public string TipoIdentificacion { get; set; }
+        public DateTime FechaNacimiento { get; set; }        
+                
+        public string SituacionAlumnoAltaOBaja() {
+            throw new NotImplementedException("Dominio.Appsogui.SituacionAlumnoAltaOBaja");
+        }
+        void BajaAlumno()
+        {
+            throw new NotImplementedException("Dominio.Appsogui.BajaAlumno");
+        }
+
+        void ReactivarAlumno()
+        {
+            throw new NotImplementedException("Dominio.Appsogui.ReactivarAlumno");
+        }
+
+        void AddCinturon(Cinturon cinturon)
+        {
+            if (cinturon == null)   { throw new ArgumentException("Cinturon"); }
+
+        }
+    }
+}
---
+++ Apsogui.Dominio/Servicios/MovimientoAlumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Apsogui
+{
+    class MovimientoAlumno
+    {
+        private string m_IdAlumno="";        
+        private int m_TipoMovimiento = 0;
+        private DateTime? m_FechaMovimiento = null;
+
+        public MovimientoAlumno(string idAlumno, TipoMovimiento tipoMovimiento, DateTime fechaMovimiento)
+        {
+            m_IdAlumno = idAlumno;
+            m_TipoMovimiento = (int)tipoMovimiento;
+            m_FechaMovimiento = fechaMovimiento;
+        }
+
+        public enum TipoMovimiento {Baja=1,Alta=2}
+
+
+    }
+
+
+}
